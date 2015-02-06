0b2d675c3e30f767fef3f73a53c948eca829bb2e
Charlie <charliewang92@gmail.com>
2015-02-05 22:56:54 -0800

    First push 132b

---
+++ CSE132BProjectDB/src/cse132B/Cse132B.java
@@ -0,0 +1,36 @@
+package cse132B;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Cse132B
+ */
+//URL of /CSE132B is a blank document. 
+@WebServlet("/Cse132B") 
+public class Cse132B extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Do it for now");
+		PrintWriter pw = response.getWriter(); 
+		pw.write("<html> <h3> first servlet running </h3> </html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
